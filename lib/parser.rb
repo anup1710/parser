435ee2c36a7231ccc16f3a2a9f25f95ecf77e31c
Anup Pareek <anup.pareek@simplybusiness.co.uk>
2021-02-21 16:15:46 +0000

    validate the file format agains supported formats

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,7 +2,10 @@
 
 # loads the file from given path
 class Parser
+  SUPPORTED_FILE_FORMATS = ['.log']
+
   def initialize(path)
     raise "file not found: #{path}" unless File.exist?(path)
+    raise "File format not allowed. Only '.log' extension files allowed" unless SUPPORTED_FILE_FORMATS.include?(File.extname(path))
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,4 +7,15 @@ describe Parser do
       described_class.new(unknown_file_path)
     end.to raise_error("file not found: #{unknown_file_path}")
   end
+
+  it 'throws an error when the file format is not supported' do
+    incorrect_format_file = File.join(RSPEC_ROOT, 'fixtures/incorrect_format.json')
+    expect { described_class.new(incorrect_format_file) }
+      .to raise_error("File format not allowed. Only '.log' extension files allowed")
+  end
+
+  it 'does not throw error for valid format file' do
+    valid_format_file = File.join(RSPEC_ROOT, 'fixtures/valid_format.log')
+    expect { described_class.new(valid_format_file) }.not_to raise_error
+  end
 end
