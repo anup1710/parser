78a2ee977692d39f4a6029bfc228d177506f5daa
Anup Pareek <anup.pareek@simplybusiness.co.uk>
2021-02-21 16:20:44 +0000

    refactoring validations

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
-# loads the file from given path
+# loads the file from given path and validates
 class Parser
-  SUPPORTED_FILE_FORMATS = ['.log']
+  SUPPORTED_FILE_FORMATS = ['.log'].freeze
 
   def initialize(path)
-    raise "file not found: #{path}" unless File.exist?(path)
-    raise "File format not allowed. Only '.log' extension files allowed" unless SUPPORTED_FILE_FORMATS.include?(File.extname(path))
+    @path = path
+    validate!
+  end
+
+  private
+
+  def validate!
+    validate_file_presence
+    validate_file_format
+  end
+
+  def validate_file_presence
+    raise "file not found: #{@path}" unless File.exist?(@path)
+  end
+
+  def validate_file_format
+    return if SUPPORTED_FILE_FORMATS.include?(File.extname(@path))
+
+    raise "File format not allowed. Only '.log' extension files allowed"
   end
 end
